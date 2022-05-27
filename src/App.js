ab23ca87fc66141fcfb72e33ae734f5bb76c3399
Mayank <cse200001043@iiti.ac.in>
2022-05-27 20:30:15 +0530

    added dashboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./components/Auth/LoginPage";
 import { SignupPage } from "./components/Auth/Signup";
 import { Home } from "./components/Home/Home";
+import { MentorDashboard } from "./components/Mentor/MentorDashboard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<LoginPage />} />
             <Route exact path="/signup" element={<SignupPage />} />
+            <Route exact path="/mentorDashboard" element={<MentorDashboard />} />
           </Routes>
         </div>
       </Router>
