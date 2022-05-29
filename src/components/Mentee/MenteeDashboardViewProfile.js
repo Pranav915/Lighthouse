ab6a13024757b09190d8c6a353d11db03a28a013
Mayank <cse200001043@iiti.ac.in>
2022-05-29 21:35:49 +0530

    finished work of profile page

--- src/components/Mentee/MenteeDashboardViewProfile.js
+++ src/components/Mentee/MenteeDashboardViewProfile.js
@@ -1,11 +1,11 @@
 import React from "react";
-import './MentorDashboardViewProfile.scss';
+import './MenteeDashboardViewProfile.scss';
 import { Helmet } from "react-helmet";
 import { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { set } from "mongoose";
 
-export const MentorDashboardViewProfile = () => {
+export const MenteeDashboardViewProfile = () => {
   const navigate = useNavigate();
   const [profile, setProfile] = useState({
     name: "",
@@ -30,7 +30,7 @@ export const MentorDashboardViewProfile = () => {
 
 
   const onsele1 = async (event) => {
-    const response = await fetch("http://localhost:5000/api/viewprofile", {
+    const response = await fetch("http://localhost:5000/api/viewprofilementee", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -66,7 +66,7 @@ export const MentorDashboardViewProfile = () => {
 
   const onsele = async (event) => {
     
-    const response = await fetch("http://localhost:5000/api/updateprofile", {
+    const response = await fetch("http://localhost:5000/api/updateprofilementee", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -182,7 +182,7 @@ export const MentorDashboardViewProfile = () => {
 
   return (
     <> 
-<div className="MentorDashboardViewProfile">
+<div className="MenteeDashboardViewProfile">
 <body>
 <div className="container" style={{'minHeight':'100%','margin':'50px 0px'}}>
 <div className="border-right" style={{'display':'flex'}}>
